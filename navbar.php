7529518efa6a51ba3cd6107c283ebe42474bfb9a
tfk891 <161776967+tfk891@users.noreply.github.com>
2025-08-13 15:17:30 +0600

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -6,7 +6,7 @@ session_start();
     <div class="top-actions">
         <?php if (isset($_SESSION['user'])): ?>
             <span class="welcome">Hello, <?php echo htmlspecialchars($_SESSION['user']); ?></span>
-            <a class="link" href="add_book.php">Add Book</a>
+            <a class="link" href="books.php">Add Book</a>
             <!-- Removed the List link as requested -->
             <a class="link" href="logout.php">Logout</a>
         <?php else: ?>
